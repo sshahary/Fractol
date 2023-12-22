0dc518a06e349410ba2e1baba81df199a5c5f7c7
sshahary <145033155+sshahary@users.noreply.github.com>
2023-12-22 17:56:09 +0100

    drawed mandelbrotset

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: sshahary <sshahary@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 11:07:26 by sshahary          #+#    #+#             */
-/*   Updated: 2023/12/22 16:34:41 by sshahary         ###   ########.fr       */
+/*   Updated: 2023/12/22 17:46:32 by sshahary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@
 # include "../lib/MLX42/include/MLX42/MLX42.h"
 # include "../mylib/ft_print/ft_printf.h"
 
-# define WIDTH 800
-# define HEIGHT 600
-# define MAX_ITER 1000
+# define WIDTH 1000
+# define HEIGHT 1000
+# define MAX_ITER 100
 
 typedef struct s_complex
 {
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sshahary <sshahary@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 11:09:36 by sshahary          #+#    #+#             */
-/*   Updated: 2023/12/22 16:36:17 by sshahary         ###   ########.fr       */
+/*   Updated: 2023/12/22 17:54:14 by sshahary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,37 +16,72 @@ int	mandelbrot(t_complex c, t_complex z, int iteration)
 {
 	t_complex	z_next;
 
+	if (iteration == MAX_ITER
+		|| ft_sqrt(z.real * z.real + z.imag * z.imag) > 4.0)
+		return (iteration);
 	z_next.real = z.real * z.real - z.imag * z.imag + c.real;
 	z_next.imag = 2.0 * z.real * z.imag + c.imag;
-	if (iteration == MAX_ITER
-		|| ft_sqrt(z.real * z.real + z.imag * z.imag) >= 4.0)
-		return (iteration);
 	return (mandelbrot(c, z_next, iteration + 1));
 }
 
-// void	drawMandelbrot(void)
-// {
-// 	for (int y = 0; y < HEIGHT; y++) {
-// 		for (int x = 0; x < WIDTH; x++)
-// 		{
-// 			t_complex c, z;
-// 			c.real = (x - WIDTH / 2.0) * 4.0 / WIDTH;
-// 			c.imag = (y - HEIGHT / 2.0) * 4.0 / HEIGHT;
-// 			z.real = 0;
-// 			z.imag = 0;	
-// 			int iteration = mandelbrot(c, z, 0);
-// 			// Map the iteration count to a color
-// 			int r = (iteration % 256);
-// 			int g = (iteration % 256);
-// 			int b = (iteration % 256);
-// 			printf("%3d %3d %3d ", r, g, b);
-// 		}
-// 		printf("\n");
-// 	}
-// }
+void	draw_mandelbrot(mlx_image_t *img)
+{
+	int			x;
+	int			y;
+	t_complex	c;
+	t_complex	z;
+	int			color;
+
+	y = 0;
+	while (y < HEIGHT)
+	{
+		x = 0;
+		while (x < WIDTH)
+		{
+			c.real = (x - WIDTH / 2.0) / (WIDTH / 4.0);
+			c.imag = (y - HEIGHT / 2.0) / (HEIGHT / 4.0);
+			z.real = 0;
+			z.imag = 0;
+			color = mandelbrot(c, z, 0) * 255 / MAX_ITER;
+			// Assuming 32-bit pixels with an alpha channel
+			uint32_t pixel_offset = (y * img->width + x);
+			img->pixels[pixel_offset * 4] = color;
+			img->pixels[pixel_offset * 4 + 1] = color;
+			img->pixels[pixel_offset * 4 + 2] = color;
+			img->pixels[pixel_offset * 4 + 3] = 255; // Alpha channel
+			x++;
+		}
+		y++;
+	}
+}
 
 int	main(void)
 {
-	ft_printf("hey");
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+
+	mlx = mlx_init(WIDTH, HEIGHT, "Fractol", true);
+	if (!mlx)
+	{
+		ft_printf("Error: Failed to initialize mlx.\n");
+		return (1);
+	}
+	img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	if (!img)
+	{
+		ft_printf("Error: Failed to create image.\n");
+		return (1);
+	}
+
+	draw_mandelbrot(img);
+
+	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+		ft_printf("ERROR");
+	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+
+	return (0);
 }
+
 

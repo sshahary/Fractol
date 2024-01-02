3babc712e39e9b69009882c6ba5228991d7b81bf
sshahary <145033155+sshahary@users.noreply.github.com>
2024-01-02 16:32:58 +0100

    added

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: sshahary <sshahary@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 11:07:26 by sshahary          #+#    #+#             */
-/*   Updated: 2023/12/22 17:46:32 by sshahary         ###   ########.fr       */
+/*   Updated: 2024/01/02 16:31:29 by sshahary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,52 @@
 typedef struct s_complex
 {
 	double	real;
-	double	imag;
+	double	i;
 }	t_complex;
 
-int	ft_sqrt(unsigned int n);
+typedef enum sets
+{
+	MANDELBROT,
+	JULIA,
+	TRICORN,
+	MANDELBROT2,
+	JULIA2,
+	TRICORN2,
+}	t_sets;
+
+
+typedef struct s_fractal
+{
+	char			*name;
+	int				id;
+	mlx_image_t		*img;
+	void			*mlx;
+	double			xmin;
+	double			xmax;
+	double			ymin;
+	double			ymax;
+	double			xzoom;
+	double			yzoom;
+	double			zoom_factor;
+	t_complex		c;
+	double			escape_value;
+	double			initial_zoom;
+	double			zoom;
+	double			x_shift;
+	double			y_shift;
+	int				mouse_x;
+	int				mouse_y;
+	unsigned int	iterations;
+	unsigned int	color;
+	unsigned int	color2;
+	unsigned int	r;
+	unsigned int	g;
+	unsigned int	b;
+}				t_fractal;
+
+void	update_render(t_fractal *fractal);
+void	fractal_init(t_fractal *fractal, int id, double c_x, double c_y);
+void	select_fractal(t_fractal *fractal);
+
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,76 +6,49 @@
 /*   By: sshahary <sshahary@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 11:09:36 by sshahary          #+#    #+#             */
-/*   Updated: 2023/12/27 11:03:41 by sshahary         ###   ########.fr       */
+/*   Updated: 2024/01/02 16:27:39 by sshahary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-int	mandelbrot(t_complex c, t_complex z, int iteration)
+static bool	check_input(int argc, char **argv, t_fractal *fractal)
 {
-	t_complex	z_next;
-
-	if (iteration == MAX_ITER
-		|| ft_sqrt(z.real * z.real + z.imag * z.imag) > 4.0)
-		return (iteration);
-	z_next.real = z.real * z.real - z.imag * z.imag + c.real;
-	z_next.imag = 2.0 * z.real * z.imag + c.imag;
-	return (mandelbrot(c, z_next, iteration + 1));
+	if (argc == 2 && !strncmp(argv[1], "mandelbrot", 10))
+		fractal_init(fractal, MANDELBROT, 0, 0);
+	else if (argc == 4 && !strncmp(argv[1], "julia", 5))
+	{
+		if (ft_atod(argv[2]) >= -2.0 && ft_atod(argv[2]) <= 2.0
+			&& ft_atod(argv[3]) >= -2.0 && ft_atod(argv[3]) <= 2.0)
+			fractal_init(fractal, JULIA, ft_atod(argv[2]), ft_atod(argv[3]));
+		else
+			return (false);
+	}
+	else if (argc == 2 && !strncmp(argv[1], "tricorn", 7))
+		fractal_init(fractal, TRICORN, 0, 0);
+	else
+		return (false);
+	return (true);
 }
 
-void	draw_mandelbrot(mlx_image_t *img)
+int	main(int argc, char **argv)
 {
-	int			x;
-	int			y;
-	t_complex	c;
-	t_complex	z;
-	int			color;
+	t_fractal		fractal;
 
-	y = 0;
-	while (y < HEIGHT)
+	if (check_input(argc, argv, &fractal))
 	{
-		x = 0;
-		while (x < WIDTH)
-		{
-			c.real = (x - WIDTH / 2.0) / (WIDTH / 4.0);
-			c.imag = (y - HEIGHT / 2.0) / (HEIGHT / 4.0);
-			z.real = 0;
-			z.imag = 0;
-			color = mandelbrot(c, z, 0) * 255 / MAX_ITER;
-			uint32_t pixel_offset = (y * img->width + x);
-			img->pixels[pixel_offset * 4] = color;
-			img->pixels[pixel_offset * 4 + 1] = color;
-			img->pixels[pixel_offset * 4 + 2] = color;
-			img->pixels[pixel_offset * 4 + 3] = 255;
-			x++;
-		}
-		y++;
+		update_render(&fractal);
+		mlx_loop_hook(fractal.mlx, keyhook, &fractal);
+		mlx_scroll_hook(fractal.mlx, &scrollhook, &fractal);
+		mlx_cursor_hook(fractal.mlx, &cursorhook, &fractal);
+		mlx_loop(fractal.mlx);
 	}
-}
-
-int	main(void)
-{
-	mlx_t		*mlx;
-	mlx_image_t	*img;
-
-	mlx = mlx_init(WIDTH, HEIGHT, "Fractol", true);
-	if (!mlx)
+	else
 	{
-		ft_printf("Error: Failed to initialize mlx.\n");
-		return (1);
+		param_error();
+		return (EXIT_FAILURE);
 	}
-	img = mlx_new_image(mlx, WIDTH, HEIGHT);
-	if (!img)
-	{
-		ft_printf("Error: Failed to create image.\n");
-		return (1);
-	}
-	draw_mandelbrot(img);
-	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
-		ft_printf("ERROR");
-	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
+	mlx_delete_image(fractal.mlx, fractal.img);
+	mlx_terminate(fractal.mlx);
 	return (EXIT_SUCCESS);
 }
